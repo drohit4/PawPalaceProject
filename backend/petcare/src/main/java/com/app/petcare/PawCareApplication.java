db83a2b940e079257caf891142750d5c19dbb7b5
drohit4 <deshmukhrohit773@gmail.com>
2025-01-01 20:20:29 +0530

    Backend Initialize - Initial Commit

---
+++ backend/petcare/src/main/java/com/app/petcare/PawCareApplication.java
@@ -0,0 +1,13 @@
+package com.app.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawCareApplication.class, args);
+	}
+
+}
